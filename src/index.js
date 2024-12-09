dcc6bb83287419275a8d15180f7bec38ac8a49a5
Eagle-lucid <vicokefie@gmail.com>
2024-12-09 02:59:43 -0500

    add: a window object is added to achieve it globally

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
-import generateContent from "./home.js";
+
+import  generateContent from "./home.js";
 generateContent();
+
+// Function to scroll to section by ID (global function)
+window.scrollToSection = function(sectionId) {
+    const section = document.getElementById(sectionId);
+    if (section) {
+        section.scrollIntoView({ behavior: 'smooth' });
+    }
+};
