245a380e20e56f0e400c542161fda58efa167477
Eagle-lucid <vicokefie@gmail.com>
2024-12-09 23:48:26 -0500

    feat: import the function of the navigation menu

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-
-import  generateContent from "./home.js";
+//generate content and navigate globally
+import  {generateContent, navigateToMenu} from "./home.js";
 generateContent();
+window.navigateToMenu = navigateToMenu;
 
 // Function to scroll to section by ID (global function)
 window.scrollToSection = function(sectionId) {
